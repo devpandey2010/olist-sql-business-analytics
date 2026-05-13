baab71349e42f84483e0447b6d79ba9370c558e6
devpandey2010 <pandeydev2010@gmail.com>
2026-05-13 12:18:12 +0530

    Chages made

--- sql/practic.sql
+++ sql/practic.sql
@@ -98,7 +98,7 @@ tied_categories AS (
     FROM ranked
     WHERE rn = 1
     GROUP BY product_category_name
-    HAVING COUNT(*) > 1  -- ← more than one seller at rank 1
+    HAVING COUNT(*) > 1  --  more than one seller at rank 1
 )
 -- Final: return all rank-1 sellers from those tied categories
 SELECT 
@@ -150,8 +150,7 @@ Filters AS(
 select r.customer_unique_id,
 r.order_id from ranked r join 
 Filters f on r.customer_unique_id=f.customer_unique_id
-where f.total_orders=1  --exactly 1 order 
-and r.rn=1;        --get that one order row
+where f.total_orders=1;  --exactly 1 order        --get that one order row
 
 /*Rank sellers by avg review score (desc). For same avg score, use total orders as tiebreaker
 (desc). Use DENSE_RANK.*/
@@ -191,3 +190,80 @@ where rn=1;
 
 /* Row number cannot be used because row number assign unique value even
 both value are same but in this we are asked to return two values if their is tie at the top*/
+
+--PRACTICE DAY 2 --LAG/LEAD/PERCENTILE FUNCTION
+
+/*For each month, calculate the total revenue and the revenue from the previous month. Show the
+absolute change and whether revenue 'Increased', 'Decreased', or 'Same' compared to previous
+month. First month should show NULL for previous revenue.*/
+--Return  month, total_revenue, prev_month_revenue, change
+with revenue_details as(
+    select strftime('%Y-%m',o.order_purchase_timestamp) as year_month,
+    round(sum(op.payment_value),2) as total_revenue from orders o join order_payments op
+    on o.order_id=op.order_id
+    group by year_month
+    order by year_month
+),
+Previous_details as(
+    select *,
+    coalesce(Lag(total_revenue,1)over(order by year_month),0) as prev_month_revenue,
+    round(coalesce(total_revenue-Lag(total_revenue,1)over(order by year_month),0),2) as diff_in_revenue,
+    case
+    when coalesce(Lag(total_revenue,1)over(order by year_month),0)=0 THEN "No Previous Data"
+        when total_revenue-Lag(total_revenue,1)over(order by year_month) >0 then "Increased"
+        when total_revenue-Lag(total_revenue,1)over(order by year_month) <0 then "Decreased"   
+        else  "same"
+        end as revenue_health
+    from revenue_details
+)
+select * from previous_details;
+
+/*For each customer, find the number of days between their consecutive orders. Return the customer,
+both order dates, and days gap. Exclude the first order of each customer (no previous order to
+compare). Order by customer and order date.
+RETURN customer_unique_id, order_date, prev_order_date, days_between_order*/
+
+ select c.customer_unique_id,
+    strftime("%Y-%m",o.order_purchase_timestamp) as order_date
+    from customers c join orders o on c.customer_id=o.customer_id
+    group by customer_unique_id;
+    
+with customer_details AS(
+    select c.customer_unique_id,
+    strftime("%Y-%m-%d",o.order_purchase_timestamp) as order_date
+    from customers c join orders o on c.customer_id=o.customer_id
+
+),
+previous_order AS(
+    select *,Lag(order_date,1)over(partition by customer_unique_id order by order_date) as previous_order_date,
+    Julianday(order_date)-Julianday(Lag(order_date,1)over(partition by customer_unique_id order by order_date))as days_between_orders
+    from customer_details
+
+)
+select * from previous_order
+where days_between_orders is not null;
+
+
+/*Find all months where revenue dropped by more than 20% compared to the previous month. Return
+the month, current revenue, previous revenue, and the percentage drop. Order by drop percentage
+descending.
+RETURN month, revenue, prev_revenue, pct_drop*/
+with revenue_details AS(
+    select strftime('%Y-%m',o.order_purchase_timestamp) as current_month,
+    round(sum(op.payment_value),2) as current_revenue
+    from orders o join order_payments op on o.order_id=op.order_id
+    group by current_month
+),
+previous_details as(
+    select *,
+    Lag(current_revenue)over(order by current_month) as prev_revenue
+    from revenue_details
+)
+select * ,CASE
+    WHEN prev_revenue = 0 OR prev_revenue IS NULL THEN NULL
+    ELSE ROUND((prev_revenue - current_revenue) * 100.0 / prev_revenue, 2)
+END AS percentage_drop from previous_details
+where percentage_drop>20.0 and prev_revenue is not null
+order by percentage_drop desc;
+
+
